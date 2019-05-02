ea51dd51b6877ca69c6a45494c8ce0248610644d
Isaac Sunoo <isaacsunoo@gmail.com>
2019-05-01 21:31:30 -0600

    fixed naming conventions

--- db/seeds/dev/teams.js
+++ db/seeds/dev/teams.js
@@ -1,28 +1,13 @@
 const teamsData = require('../../teamsData');
-const createTeam = (knex, team) => {
+const createTeam = (knex, teams) => {
   return knex('teams')
     .insert(
       {
-        teamName: team.team_name,
-        location: team.location,
-        homeRink: team.home_rink
+        teamName: teams.team_name,
+        location: teams.location,
+        homeRink: teams.home_rink
       },
       'id'
     )
 };
 
-// exports.seed = (knex, Promise) => {
-//   return knex('footnotes')
-//     .del() // delete footnotes first
-//     .then(() => knex('papers').del()) // delete all papers
-//     .then(() => {
-//       let paperPromises = [];
-
-//       papersData.forEach(paper => {
-//         paperPromises.push(createPaper(knex, paper));
-//       });
-
-//       return Promise.all(paperPromises);
-//     })
-//     .catch(error => console.log(`Error seeding data: ${error}`));
-// };
