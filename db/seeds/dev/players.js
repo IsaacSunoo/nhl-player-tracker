476f3380e7d6e57f758f883f38e85bb8afaa5945
Isaac Sunoo <isaacsunoo@gmail.com>
2019-05-01 22:06:20 -0600

    updated seeding database

---
+++ db/seeds/dev/players.js
@@ -0,0 +1,28 @@
+const playersData = require('../../playersData');
+const createPlayers = (knex, player) => {
+  return knex('players').insert(
+    {
+      number: player.number,
+      name: player.name,
+      position: player.position,
+      age: player.age,
+      teamId: player.teamId
+    },
+    'id'
+  );
+};
+
+exports.seed = (knex, Promise) => {
+  return knex('players')
+    .del()
+    .then(() => {
+      let playersPromises = [];
+
+      playersData.forEach(player => {
+        playersPromises.push(createPlayers(knex, player));
+      });
+
+      return Promise.all(playersPromises);
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
--- db/seeds/dev/teams.js
+++ db/seeds/dev/teams.js
@@ -1,13 +1,28 @@
 const teamsData = require('../../teamsData');
-const createTeam = (knex, teams) => {
+const createTeam = (knex, team) => {
   return knex('teams')
     .insert(
       {
-        teamName: teams.team_name,
-        location: teams.location,
-        homeRink: teams.home_rink
+        teamName: team.team_name,
+        location: team.location,
+        homeRink: team.home_rink
       },
       'id'
     )
+};
+
+exports.seed = (knex, Promise) => {
+  return knex('teams')
+    .del()
+    .then(() => {
+      let teamsPromises = [];
+
+      teamsData.forEach(team => {
+        teamsPromises.push(createTeam(knex, team));
+      });
+
+      return Promise.all(teamsPromises);
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
 };
 
